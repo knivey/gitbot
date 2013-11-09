a4f6c87d257c3ab010c9944425302ffdb8c7c5e4
knivey <knivey@botops.net>
2013-11-09 05:53:22 -0500

    changing things of course

--- gitbot.go
+++ gitbot.go
@@ -13,23 +13,13 @@ import (
 
 var (
 	Endpoint *data.DataEndpoint
-	Port     string
-	Chan     string
+	Channel  string
 )
 
-type Payload struct {
-	Before      string
-	After       string
-	Ref         string
-	Compare     string
-	Created     bool
-	Deleted     bool
-	Forced      bool
-	Commits     []*Commit
-	Head_commit *Commit
-	Pusher      *Person //may not have Username, beware
-	Repository  *Repository
-}
+const (
+	MSG1 = "[%s] %s pushed %v commits to %s %s"
+	MSG2 = "[%s/%s] %s: %s: %s"
+)
 
 type Commit struct {
 	Added     []string
@@ -42,6 +32,30 @@ type Commit struct {
 	Message   string
 	Timestamp string //Might be able to use time here
 	Url       string
+}
+
+func (c Commit) User() string {
+	if c.Committer == nil || c.Committer.Username == "" {
+		return "\x02Unknown\x02"
+	}
+	return c.Committer.Username
+}
+
+func (c Commit) ShortId() string {
+	if c.Id == "" {
+		return "\x02Unknown\x02"
+	}
+	if len(c.Id) < 7 {
+		return c.Id
+	}
+	return c.Id[:7]
+}
+
+func (c Commit) Msg() string {
+	if c.Message == "" {
+		return "No message"
+	}
+	return c.Message
 }
 
 type Person struct {
@@ -73,25 +87,64 @@ type Repository struct {
 	Watchers      uint
 }
 
-func alertChan(pl Payload) {
-	name := pl.Repository.Name
-	num := len(pl.Commits)
-	who := pl.Pusher.Name
+type Payload struct {
+	Before      string
+	After       string
+	Ref         string
+	Compare     string
+	Created     bool
+	Deleted     bool
+	Forced      bool
+	Commits     []*Commit
+	Head_commit *Commit
+	Pusher      *Person //may not have Username, beware
+	Repository  *Repository
+}
+
+func (pl Payload) Branch() string {
 	refs := strings.Split(pl.Ref, "/")
-	branch := refs[len(refs)-1]
+	return refs[len(refs)-1]
+}
+
+func (pl Payload) Name() string {
+	if pl.Repository == nil || pl.Repository.Name == "" {
+		return "\x02Unknown\x02"
+	}
+	return pl.Repository.Name
+}
+
+func (pl Payload) NumCommits() int {
+	if pl.Commits == nil {
+		return 0
+	}
+	return len(pl.Commits)
+}
+
+func (pl Payload) PusherName() string {
+	if pl.Pusher == nil || pl.Pusher.Name == "" {
+		return "\x02Unknown\x02"
+	}
+	return pl.Pusher.Name
+}
+
+func alertChan(pl Payload) {
+	name := pl.Name()
+	num := pl.NumCommits()
+	who := pl.PusherName()
+	branch := pl.Branch()
 	comp, err := gitio.Shorten(pl.Compare)
 	if err != nil {
 		comp = pl.Compare
 	}
 
-	msg := fmt.Sprintf("[%s] %s pushed %v commits to %s %s", name, who, num, branch, comp)
-	Endpoint.Privmsg(Chan, msg)
+	msg := fmt.Sprintf(MSG1, name, who, num, branch, comp)
+	Endpoint.Privmsg(Channel, msg)
 
 	var c *Commit
 	for i := 0; i < num; i++ {
 		c = pl.Commits[i]
-		msg = fmt.Sprintf("[%s/%s] %s: %s: %s", name, branch, c.Id[:7], c.Committer.Username, c.Message)
-		Endpoint.Privmsg(Chan, msg)
+		msg = fmt.Sprintf(MSG2, name, branch, c.ShortId(), c.User(), c.Message)
+		Endpoint.Privmsg(Channel, msg)
 	}
 }
 
@@ -101,15 +154,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	err := json.Unmarshal([]byte(payload), &Pl)
 	if err != nil {
 		log.Println(err)
+		return
 	}
+	alertChan(Pl)
 }
 
 //Start the http listener
-func Listen() (err error) {
+func Listen(port, channel string, endpoint *data.DataEndpoint) (err error) {
+	Channel = channel
+	Endpoint = endpoint
 	http.HandleFunc("/", handler)
-	err = http.ListenAndServe(":"+Port, nil)
-	if err != nil {
-		return err
-	}
-	return nil
+	err = http.ListenAndServe(":"+port, nil)
+	return
 }
